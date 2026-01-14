9df75909fd1ddaf85b1def9d44d2b4c56c9d1065
曾士珍 <sjtseng0924@gpu7.cplab.cs.nycu.edu.tw>
2026-01-14 21:30:04 +0800

    add: fixed noise config and updates

--- train/script/train.sh
+++ train/script/train.sh
@@ -1,14 +1,13 @@
-export OMINI_CONFIG=/home/chchen/lab/OminiControl_test/train/config/config.yaml
-export CUDA_VISIBLE_DEVICES=5
+export OMINI_CONFIG=/home/sjtseng0924/OminiControl_test/train/config/config.yaml
+export CUDA_VISIBLE_DEVICES=2
 # *[Specify the WANDB API key]
-export WANDB_API_KEY='your_wandb_api_key_here'
+export WANDB_API_KEY='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'
 
 echo $OMINI_CONFIG
 export TOKENIZERS_PARALLELISM=true
 
 # Ensure the repo is on PYTHONPATH so `import omini` works
-export PYTHONPATH=/home/chchen/lab/OminiControl_test:${PYTHONPATH}
-
-cd /home/chchen/lab/OminiControl_test
+export PYTHONPATH=/home/sjtseng0924/OminiControl_test:${PYTHONPATH}
+cd /home/sjtseng0924/OminiControl_test
 # For single-GPU or multi-GPU with accelerate; switch module to train_custom
 accelerate launch --num_processes=1 --mixed_precision=bf16 --main_process_port 41358 -m omini.train_flux.train_custom
